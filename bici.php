7001172e87c7ffcb69cc4a486ee0f121c6222ddc
acomai <adriano-liste@fastwebnet.it>
2017-04-06 22:13:50 +0200

    Modifiche 6-4-2017

--- bici.php
+++ bici.php
@@ -76,28 +76,114 @@ function create_post_type_percorso() {
 			)
 			);
 }
+add_action( 'init', 'act_nodo_init' );
+/**
+ * Register a nodo post type.
+ *
+ * @link http://codex.wordpress.org/Function_Reference/register_post_type
+ */
+function act_nodo_init() {
+	$labels = array(
+			'name'               => _x( 'act_Nodi', 'post type general name'),
+			'singular_name'      => _x( 'act_Nodo', 'post type singular name'),
+			'menu_name'          => _x( 'act_Nodi', 'admin menu'),
+			'name_admin_bar'     => _x( 'act_Nodo', 'add new on admin bar'),
+			'add_new'            => _x( 'Aggiungi', 'act_nodo' ),
+			'add_new_item'       => __( 'Add New act_Nodo'),
+			'new_item'           => __( 'Nuovo act_Nodo'),
+			'edit_item'          => __( 'Edit act_Nodo'),
+			'view_item'          => __( 'View act_Nodo'),
+			'all_items'          => __( 'Tutti gli act_Nodi'),
+			'search_items'       => __( 'Search act_Nodi'),
+			'parent_item_colon'  => __( 'Parent act_Nodi:'),
+			'not_found'          => __( 'No nodi found.'),
+			'not_found_in_trash' => __( 'No nodi found in Trash.')
+	);
+	
+	$args = array(
+			'labels'             => $labels,
+			'description'        => __( 'Nodo percorribile.'),
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'rewrite'            => array( 'slug' => 'nodi' ),
+			'capability_type'    => 'post',
+			'has_archive'        => true,
+			'hierarchical'       => false,
+			'menu_position'      => null,
+			'taxonomies'         => array( 'category' ),
+			'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'custom-fields')
+	);
+	
+	register_post_type( 'act_nodo', $args );
+}
 
-
-add_action( 'init', 'slow_itinerario_init' );
+add_action( 'init', 'act_percorso_init' );
 /**
  * Register a itinerario post type.
  *
  * @link http://codex.wordpress.org/Function_Reference/register_post_type
  */
-function slow_itinerario_init() {
+function act_percorso_init() {
 	$labels = array(
-			'name'               => _x( 'Itinerari', 'post type general name'),
-			'singular_name'      => _x( 'Itinerario', 'post type singular name'),
-			'menu_name'          => _x( 'Itinerari', 'admin menu'),
-			'name_admin_bar'     => _x( 'Itinerario', 'add new on admin bar'),
-			'add_new'            => _x( 'Aggiungi', 'itinerario' ),
-			'add_new_item'       => __( 'Add New Itinerario'),
-			'new_item'           => __( 'Nuovo Itinerario'),
-			'edit_item'          => __( 'Edit Itinerario'),
-			'view_item'          => __( 'View Itinerario'),
-			'all_items'          => __( 'Tutti gli Itinerari'),
-			'search_items'       => __( 'Search Itinerari'),
-			'parent_item_colon'  => __( 'Parent Itinerari:'),
+			'name'               => _x( 'act_Percorsi', 'post type general name'),
+			'singular_name'      => _x( 'act_Percorso', 'post type singular name'),
+			'menu_name'          => _x( 'act_Percorsi', 'admin menu'),
+			'name_admin_bar'     => _x( 'act_Percorso', 'add new on admin bar'),
+			'add_new'            => _x( 'Aggiungi', 'act_percorso' ),
+			'add_new_item'       => __( 'Add New act_Percorso'),
+			'new_item'           => __( 'Nuovo act_Percorso'),
+			'edit_item'          => __( 'Edit act_Percorso'),
+			'view_item'          => __( 'View act_Percorso'),
+			'all_items'          => __( 'Tutti gli act_Percorsi'),
+			'search_items'       => __( 'Search act_Percorsi'),
+			'parent_item_colon'  => __( 'Parent act_Percorsi:'),
+			'not_found'          => __( 'No percorsi found.'),
+			'not_found_in_trash' => __( 'No percorsi found in Trash.')
+	);
+	
+	$args = array(
+			'labels'             => $labels,
+			'description'        => __( 'Percorso percorribile.'),
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'rewrite'            => array( 'slug' => 'percorsi' ),
+			'capability_type'    => 'post',
+			'has_archive'        => true,
+			'hierarchical'       => false,
+			'menu_position'      => null,
+			'taxonomies'         => array( 'category' ),
+			'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'custom-fields')
+	);
+	
+	register_post_type( 'act_percorso', $args );
+}
+
+add_action( 'init', 'act_itinerario_init' );
+/**
+ * Register a itinerario post type.
+ *
+ * @link http://codex.wordpress.org/Function_Reference/register_post_type
+ */
+function act_itinerario_init() {
+	$labels = array(
+			'name'               => _x( 'act_Itinerari', 'post type general name'),
+			'singular_name'      => _x( 'act_Itinerario', 'post type singular name'),
+			'menu_name'          => _x( 'act_Itinerari', 'admin menu'),
+			'name_admin_bar'     => _x( 'act_Itinerario', 'add new on admin bar'),
+			'add_new'            => _x( 'Aggiungi', 'act_itinerario' ),
+			'add_new_item'       => __( 'Add New act_Itinerario'),
+			'new_item'           => __( 'Nuovo act_Itinerario'),
+			'edit_item'          => __( 'Edit act_Itinerario'),
+			'view_item'          => __( 'View act_Itinerario'),
+			'all_items'          => __( 'Tutti gli act_Itinerari'),
+			'search_items'       => __( 'Search act_Itinerari'),
+			'parent_item_colon'  => __( 'Parent act_Itinerari:'),
 			'not_found'          => __( 'No itinerari found.'),
 			'not_found_in_trash' => __( 'No itinerari found in Trash.')
 	);
@@ -110,7 +196,7 @@ function slow_itinerario_init() {
 			'show_ui'            => true,
 			'show_in_menu'       => true,
 			'query_var'          => true,
-			'rewrite'            => array( 'slug' => 'itinerario' ),
+			'rewrite'            => array( 'slug' => 'itinerari' ),
 			'capability_type'    => 'post',
 			'has_archive'        => true,
 			'hierarchical'       => false,
@@ -119,6 +205,6 @@ function slow_itinerario_init() {
 			'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'custom-fields')
 	);
 	
-	register_post_type( 'book', $args );
+	register_post_type( 'act_itinerario', $args );
 }
 ?>
